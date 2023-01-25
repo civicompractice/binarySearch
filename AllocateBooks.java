8bd67a9b2f6f971bf599ae3d43aa62fe222e8810
civicompractice <123489758+civicompractice@users.noreply.github.com>
2023-01-25 19:07:43 +0530

    Update AllocateBooks.java

--- AllocateBooks.java
+++ AllocateBooks.java
@@ -1,4 +1,30 @@
 // Time Complexity : O(N*log(n))
+// Problem Statement: Given an array of integers A of size N and an integer B.
+
+// The College library has N bags, the ith book has A[i] number of pages.
+
+// You have to allocate books to B number of students so that the maximum number of pages allocated to a student is minimum.
+
+// Conditions given :
+
+// A book will be allocated to exactly one student.
+
+// Each student has to be allocated at least one book.
+
+// Allotment should be in contiguous order, for example, A student cannot be allocated book 1 and book 3, skipping book 2.
+
+// Calculate and return the minimum possible number. Return -1 if a valid assignment is not possible.
+
+// Examples:
+
+// Example 1:
+
+// Input: A = [12, 34, 67, 90]
+//        B = 2
+
+// Output: 113
+
+// Explaination: Let’s see all possible cases of how books can be allocated for each student.
 
 import java.util.*;
 
