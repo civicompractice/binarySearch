cbd847e7d2e862c67ccef3fe329b6a7625b04760
civicompractice <123489758+civicompractice@users.noreply.github.com>
2023-01-25 19:08:37 +0530

    Update NthRoot.java

--- NthRoot.java
+++ NthRoot.java
@@ -1,7 +1,30 @@
 
 //Time Complexity : O(N*log2(M*10))
+// Problem Statement: Given two numbers N and M, find the Nth root of M.
 
+// The nth root of a number M is defined as a number X when raised to the power N equals M.
 
+// Example 1:
+
+// Input: N=3 M=27
+
+// Output: 3
+
+// Explanation: The cube root of 27 is 3.
+// Example 2:
+
+// Input: N=2 M=16
+
+// Output: 4
+
+// Explanation: The square root of 16 is 4
+// Example 3:
+
+// Input: N=5 M=243
+
+// Output: 3
+
+// Explaination: The 5th root of 243 is 3
 
 import java.util.*;
 
